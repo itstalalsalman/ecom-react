e856ddea4f3d7df43d6d3a12078246192048de74
Talal Salman Zafeer <mustangcreator@gmail.com>
2024-03-26 19:55:01 +0300

    Initial Setup Start With Navbar Implementation Done

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import logo from 'C:/Users/musta/Desktop/ecommerce-product-page-main/ecom-react/src/images/logo.svg'
+import cart from 'C:/Users/musta/Desktop/ecommerce-product-page-main/ecom-react/src/images/icon-cart.svg'
+import avatar from 'C:/Users/musta/Desktop/ecommerce-product-page-main/ecom-react/src/images/image-avatar.png'
+import './Navbar.css'
+
+const Navbar = () => {
+  return (
+    <div className='navbar-container'>
+        <img src={logo} alt="" className='logo'/>
+        <ul className='nav-items-container'>
+            <li>Collections</li>
+            <li>Men</li>
+            <li>Women</li>
+            <li>About</li>
+            <li>Contact</li>
+        </ul>
+        <div className='cart-container'>
+            <img src={cart} alt="" />
+            <img src={avatar} alt="" className='img-avatar'/>
+        </div>
+
+    </div>
+  )
+}
+
+export default Navbar
